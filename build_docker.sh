f1278ad410550b77f67aefba9733de5ec9aa8e8c
Chibundu Anaduaka <chibundu@saos-mbp.home>
2021-08-03 16:18:40 +0100

    updated files

--- build_docker.sh
+++ build_docker.sh
@@ -7,10 +7,10 @@ aws ecr-public get-login-password --region us-east-1 | docker login --username A
 docker build -t chibsapp .
 
 # Tag image
-docker tag chibsapp:latest public.ecr.aws/v4n3e9u9/chibsapp:$1
+docker tag chibsapp:latest public.ecr.aws/v4n3e9u9/chibsapp:${CIRCLE_WORKFLOW_ID:0:7}
 
 # List images
 docker image ls
 
 # Push image to ECS docker Repo
-docker push public.ecr.aws/v4n3e9u9/chibsapp:$1
+docker push public.ecr.aws/v4n3e9u9/chibsapp:${CIRCLE_WORKFLOW_ID:0:7}
